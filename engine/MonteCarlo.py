8a3b985d11c63cea1b76c03f3c86e5677f838a96
Tyler Martin <tyler.martin@nist.gov>
2016-11-24 18:03:08 -0500

    Minor Cleanup for MonteCarlo

--- engine/MonteCarlo.py
+++ engine/MonteCarlo.py
@@ -2,6 +2,14 @@ import random
 import logging
 
 class MonteCarlo(object):
+  '''
+  Base :class:`Engine` for conducting  Monte Carlo (MC) style simulations
+
+  This engine accumulates a list of MC moves which subclass :class:`MonteCarloMove`
+  and then randomly chooses from them at every timestep. After calling the moves
+  :func:`attempt` method the move records whether the move was accepted or rejected.
+
+  '''
   def __init__(self):
     super(MonteCarlo,self).__init__() #must call parent class' constructor
     self.moveList = list()
@@ -17,11 +25,24 @@ class MonteCarlo(object):
     if not (move.name in self.rates):
       self.rates[move.name] = 0
   def run(self,num_attempts,log_rate = 5):
+    '''
+    Contduct the Monte Carlo simulation.
+
+    Parameters
+    ----------
+    num_attempts : int, *Required*
+        The number of MC moves that the engine should attempt in succession before
+        stopping. 
+    log_rate : int, *Optional*
+        The rate at which the logger (at the INFO level) should log updates.
+    '''
     if self.system.box.cellList is not None:
       self.system.box.cellList.build_nlist(self.system.positions,central_origin=True)
+
     for i in range(num_attempts):
       move = random.choice(self.moveList)
       success = move.attempt()
+
       self.rates['total_attempted'] += 1
       if success:
         self.rates['total_accepted'] += 1
@@ -32,7 +53,8 @@ class MonteCarlo(object):
         rate = accepted/float(attempted)
         logStr  = 'Step {}/{}, rate: {}'.format(i,num_attempts-1,rate)
         self.logger.info(logStr)
+
     accepted = self.rates['total_accepted']
     attempted = self.rates['total_attempted']
     rate = accepted/float(attempted)
-    print 'Acceptance rate: {}/{} = {}'.format(accepted,attempted,rate)
+    self.logger.info(print 'Acceptance rate: {}/{} = {}'.format(accepted,attempted,rate))
