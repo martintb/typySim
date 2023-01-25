4d5133fc07c6c726ac251d94e9d871b0e0b38a1c
Tyler Martin <tyler.martin@nist.gov>
2023-01-25 15:24:39 -0500

    refactor: updated parent file writer class

--- typySim/writer/Writer.py
+++ typySim/writer/Writer.py
@@ -1,9 +1,13 @@
-
+import os
 
 class Writer(object):
   '''
   Base class for all writers
   '''
-  def __init__(self):
+  def __init__(self,fileBase):
     self.name = 'BaseWriter'
+    self.file = None
+    self.fileBase = fileBase
+  def write_frame(*args,**kwargs):
+    raise NotImplementedError('write_frame shouldn\'t be called on the parent class!')
   
